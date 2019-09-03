3be91a4da4dc3df23219276945fccbdccab947b0
Fernando <fernandomenjivar2011@gmail.com>
2019-09-03 06:53:40 -0600

    🎉 first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require("webpack");
+const path = require("path");
+
+const config = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 5000
+    }
+};
+
+module.exports = config;
